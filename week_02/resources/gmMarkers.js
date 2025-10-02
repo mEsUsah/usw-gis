72d1fce144e92fbd321f90c2e7272528b32f7315
mEsUsah <stanley@haxor.no>
2025-10-02 11:48:09 +0100

    fix(tutorial04): add infoWindow

--- week_02/resources/gmMarkers.js
+++ week_02/resources/gmMarkers.js
@@ -8,12 +8,27 @@ function drawMap() {
     const map = new google.maps.Map(document.getElementById("map_space"), mapOptions);
 
     // Add a marker
-    var loc = new google.maps.LatLng(51.47806, 3.1825);
     var mkrOpt = {
-        position: loc,
-        map: map
+        position: new google.maps.LatLng(51.47806, 3.1825),
+        icon: 'resources/icons/flag2.png',
+        title: 'Look, a marker!',
+        shape: {
+            coords: [1,2, 18,3, 18,14, 5,14, 5,24, 1,24],
+            type: 'poly'
+        },
+        map: map,
     }
     var mkr = new google.maps.Marker(mkrOpt);
+
+    // Add an info window to the marker
+    var infoWindowOpts = { 
+      content: '<a href="https://en.wikipedia.org/wiki/' +
+                'Liberty_Stadium">Liberty Stadium</a>' 
+    };
+    var infoWindow = new google.maps.InfoWindow(infoWindowOpts);
+    mkr.addListener('click', function() {
+        infoWindow.open(map, mkr);
+    });
 }
 
 if (typeof google === 'object' && typeof google.maps === 'object') {
