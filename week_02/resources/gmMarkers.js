7cad775457ff71b7b42afcec28915eee0434819e
mEsUsah <stanley@haxor.no>
2025-10-02 19:39:15 +0100

    fix(tutorial04): add controls and styles

--- week_02/resources/gmMarkers.js
+++ week_02/resources/gmMarkers.js
@@ -3,13 +3,64 @@ function drawMap() {
     const mapOptions = {
         center: { lat: 51.47806, lng: 3.1825 }, // Zeebrugge, Belgium
         zoom: 8,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+        // remove buttons that are not needed
+        disableDefaultUI: true,
+
+        // enable map type control
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        mapTypeControl: true,
+        mapTypeControlOptions: {
+            mapTypeIds: [
+                google.maps.MapTypeId.ROADMAP, 
+                'evilMapType', 
+            ],
+            position: google.maps.ControlPosition.TOP_LEFT,
+            style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+        },
+        
+        // enable zoom control
+        zoomControl: true,
+        zoomControlOptions: {
+            position: google.maps.ControlPosition.TOP_RIGHT
+        },
     };
     const map = new google.maps.Map(document.getElementById("map_space"), mapOptions);
 
+
+    // Add styled map types
+    var evilStyle = [
+        {
+            "featureType": "all",
+            "elementType": "geometry.fill",
+            "stylers": [
+                { "color": "#863b3b" }
+            ]
+        },
+        {
+            "featureType": "water",
+            "elementType": "geometry.fill",
+            "stylers": [
+                { "color": "#000000" }
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "geometry",
+            "stylers": [
+                { "color": "#ffff00" }
+            ]
+        }
+    ];
+    var styledMapType = new google.maps.StyledMapType(evilStyle,{
+      map: map,
+      name: 'Evil AF'
+    });
+    map.mapTypes.set('evilMapType', styledMapType);
+
+    
     // Add a marker
     var mkrOpt = {
-        position: new google.maps.LatLng(51.47806, 3.1825),
+        position: new google.maps.LatLng(51.0, 3.1825),
         icon: 'resources/icons/flag2.png',
         title: 'Look, a marker!',
         shape: {
