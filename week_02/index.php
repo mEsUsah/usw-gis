d7ca7843d58a83f30615d6532894097347a6123e
mEsUsah <stanley@haxor.no>
2025-10-16 18:29:22 +0100

    fix(week2): make index prettier

--- week_02/index.php
+++ week_02/index.php
@@ -5,21 +5,41 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>USW - AIM Week 2</title>
     <link rel="icon" type="image/svg+xml" href="/favicon.svg">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
-<body>
-    <h1>GIS and the Spatial Web</h1>
-    <p>Work during my USW studies autumn 2025</p>
-    <p>Week 2 (2025.09.29 - 2025.10.05)</p>
-    <ul>
-        <?php
-            $files = scandir(__DIR__);
-            foreach ($files as $file) {
-                // only list visible files, not directories and only files with the htm extension
-                if (!is_dir($file) && substr($file, 0, 1) !== '.' && pathinfo($file, PATHINFO_EXTENSION) === 'htm') {
-                    echo '<li><a href="' . $file . '">' . $file . '</a></li>';
+<body class="w-full max-w-4xl mx-auto">
+    <header class="flex flex-row gap-4 lg:gap-8 items-center">
+        <img src="/favicon.svg" alt="Logo for USW coursework by Stanley Skarshaug" class="h-24 lg:h-32">
+        <div class="pr-2">
+            <h1 class="text-xl lg:text-4xl font-medium">GIS and the Spatial Web</h1>
+            <p class="text-md lg:text-xl">Google Maps API</p>
+        </div>
+    </header>
+    <main class="m-4 lg:m-8">
+        <h2 class="text-lg lg:text-2xl font-medium">Tutorials</h2>
+        <p class=" mb-4">Tutorials and coursework during my USW studies autumn 2025</p>
+        <ul>
+            <?php
+                $files = scandir(__DIR__);
+                foreach ($files as $file) {
+                    // only list visible files, not directories and only files with the htm extension
+                    if (!is_dir($file) && substr($file, 0, 1) !== '.' && pathinfo($file, PATHINFO_EXTENSION) === 'htm') {
+                        
+                        // Load the HTML file and get the title tag in the file
+                        $dom = new DOMDocument();
+                        $internalErrors = libxml_use_internal_errors(true);
+                        $dom->loadHTMLFile($file);
+                        $titleTags = $dom->getElementsByTagName('title');
+
+                        // If there is a title tag, use its content as the link text, otherwise use the filename
+                        $title = $titleTags->length > 0 ? $titleTags->item(0)->textContent : $file;
+                        ?>
+                        <li>🎯<a href="<?php echo $file; ?>" class="text-red-500 hover:underline"><?php echo $title; ?></a></li>
+                        <?php
+                    }
                 }
-            }
-        ?>
-    </ul>
+            ?>
+        </ul>
+    </main>
 </body>
 </html>
