d7b42385e37aaf6187a264be6d7855358ec1064f
mEsUsah <stanley@haxor.no>
2025-10-16 11:46:22 +0100

    fix(tutorial06): add color to GeoJSON features

--- week_04/resources/gmGeoJSON.js
+++ week_04/resources/gmGeoJSON.js
@@ -1,6 +1,6 @@
 function drawMap() {
     const mapOptions = {
-        center: { lat: 59.9139, lng: 10.7522 }, // Oslo, Norway
+        center: {lat:52.0, lng:-3.0 }, // England, UK
         zoom: 7,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
@@ -12,6 +12,23 @@ function drawMap() {
             map.data.addGeoJson(data);
         });
 
+    map.data.addListener('click', (event) => {
+        console.log(event.feature.getProperty('FEATURE'));
+    });
+
+    // add colors to all features
+    map.data.setStyle((feature) => {
+        const featureName = feature.getProperty('FEATURE');
+        let color = 'red';
+        if (featureName === 'Coal bearing strata at surface') {
+            color = 'yellow';
+        }
+        return {
+            fillColor: color,
+            strokeColor: 'black',
+            strokeWeight: 1
+        };
+    });
 }
 
 if (typeof google === 'object' && typeof google.maps === 'object') {
