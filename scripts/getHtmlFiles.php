24fd8817ab790ac2859d70fab20032d1477cae4e
mEsUsah <stanley@haxor.no>
2025-10-16 22:07:04 +0100

    refactor(week2): moved getHtmlFiles intro separate file

---
+++ scripts/getHtmlFiles.php
@@ -0,0 +1,28 @@
+<?php
+
+function getHtmlFiles($directory): array
+{
+    $files = scandir($directory);
+    $output = [];
+    foreach ($files as $file) {
+        // only list visible files, not directories and only files with the htm extension
+        if (!is_dir($file) && substr($file, 0, 1) !== '.' && pathinfo($file, PATHINFO_EXTENSION) === 'htm') {
+            
+            // Load the HTML file and get the title tag in the file
+            $dom = new DOMDocument();
+            libxml_use_internal_errors(true);
+            $dom->loadHTMLFile($file);
+            $titleTags = $dom->getElementsByTagName('title');
+
+            // If there is a title tag, use its content as the link text, otherwise use the filename
+            $title = $titleTags->length > 0 ? $titleTags->item(0)->textContent : $file;
+            $output[] = [
+                'file' => $file,
+                'title' => $title
+            ];
+        }
+    }
+    return $output;
+}
+
+?>
--- week_02/index.php
+++ week_02/index.php
@@ -1,3 +1,8 @@
+<?php
+    require __DIR__ . '/../scripts/getHtmlFiles.php';
+    $files = getHtmlFiles(__DIR__);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,7 +12,7 @@
     <link rel="icon" type="image/svg+xml" href="/favicon.svg">
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
-<body class="w-full max-w-4xl mx-auto">
+<body class="w-full max-w-4xl mx-auto flex flex-col h-screen">
     <header class="flex flex-row gap-4 lg:gap-8 items-center">
         <img src="/favicon.svg" alt="Logo for USW coursework by Stanley Skarshaug" class="h-24 lg:h-32">
         <div class="pr-2">
@@ -15,31 +20,21 @@
             <p class="text-md lg:text-xl">Google Maps API</p>
         </div>
     </header>
-    <main class="m-4 lg:m-8">
+    <main class="m-4 lg:m-8 flex-grow">
         <h2 class="text-lg lg:text-2xl font-medium">Tutorials</h2>
         <p class=" mb-4">Tutorials and coursework during my USW studies autumn 2025</p>
         <ul>
             <?php
-                $files = scandir(__DIR__);
                 foreach ($files as $file) {
-                    // only list visible files, not directories and only files with the htm extension
-                    if (!is_dir($file) && substr($file, 0, 1) !== '.' && pathinfo($file, PATHINFO_EXTENSION) === 'htm') {
-                        
-                        // Load the HTML file and get the title tag in the file
-                        $dom = new DOMDocument();
-                        $internalErrors = libxml_use_internal_errors(true);
-                        $dom->loadHTMLFile($file);
-                        $titleTags = $dom->getElementsByTagName('title');
-
-                        // If there is a title tag, use its content as the link text, otherwise use the filename
-                        $title = $titleTags->length > 0 ? $titleTags->item(0)->textContent : $file;
-                        ?>
-                        <li>🎯<a href="<?php echo $file; ?>" class="text-red-500 hover:underline"><?php echo $title; ?></a></li>
-                        <?php
-                    }
+                ?>
+                    <li>🎯<a href="<?php echo $file['file']; ?>" class="text-[#DE0832] hover:underline"><?php echo $file['title']; ?></a></li>
+                <?php
                 }
             ?>
         </ul>
     </main>
+    <footer class="bg-[#DE0832] text-white py-8">
+        <p class="text-center">USW coursework by Stanley Skarshaug</p>
+    </footer>
 </body>
 </html>
