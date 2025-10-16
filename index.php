dc70f2b83bd727fb2a26f29076c86ab6f94f72c0
mEsUsah <stanley@haxor.no>
2025-10-16 23:04:01 +0100

    fix(index): apply design

--- index.php
+++ index.php
@@ -3,23 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>USW - GIS</title>
-    <link rel="icon" type="image/svg+xml" href="favicon.svg">
+    <title>USW GIS | Home</title>
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
-<body>
-    <h1>GIS and the Spatial Web</h1>
-    <p>Work during my USW studies autumn 2025</p>
-    <ul>
-        <?php
-            // list all directories
-            $files = scandir(__DIR__);
-            foreach ($files as $file) {
-                // only list visible directories
-                if (is_dir($file) && substr($file, 0, 1) !== '.') {
-                    echo '<li><a href="' . $file . '">' . $file . '</a></li>';
-                }
-            }
-        ?>
-    </ul>
+<body class="w-full max-w-4xl mx-auto flex flex-col h-screen">
+    <header class="flex flex-row gap-4 lg:gap-8 items-center">
+        <img src="/favicon.svg" alt="Logo for USW coursework by Stanley Skarshaug" class="h-24 lg:h-32">
+        <div class="pr-2">
+            <h1 class="text-xl lg:text-4xl font-medium">GIS and the Spatial Web</h1>
+            <p class="text-md lg:text-xl">Home / Overview</p>
+        </div>
+    </header>
+    <main class="m-4 lg:m-8 flex-grow">
+        <h2 class="text-lg lg:text-2xl font-medium">Tutorials</h2>
+        <p class=" mb-4">Tools discussed during my USW studies autumn 2025</p>
+        <ul>
+            <li>🎯<a href="/googleMapsApi" class="text-[#DE0832] hover:underline">Google Maps API</a></li>
+        </ul>
+
+    </main>
+    <footer class="bg-[#DE0832] text-white py-8">
+        <p class="text-center">USW coursework by Stanley Skarshaug</p>
+    </footer>
 </body>
 </html>
