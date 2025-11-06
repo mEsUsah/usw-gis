e6f912e049207517dd9a97359ce6a76e690b4d99
mEsUsah <stanley@haxor.no>
2025-11-06 11:24:03 +0000

    fix(OL-tutorial 1): center map like on the GM tutorial

--- openLayers/resources/js/olTutorial_01.js
+++ openLayers/resources/js/olTutorial_01.js
@@ -1,8 +1,9 @@
 
 map1 = new ol.Map({
     view: new ol.View({
-        center: [0, 0],
-        zoom: 2
+        // https://openlayers.org/en/v4.6.5/apidoc/ol.proj.html#.fromLonLat
+        center: ol.proj.fromLonLat([10.7522, 59.9139], 'EPSG:3857'), // Oslo, Norway, Google maps CRS
+        zoom: 7
     }),
     layers: [
         new ol.layer.Tile({
