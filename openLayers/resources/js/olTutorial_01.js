a2c6606c19dd57a1054564e9ce1c6b2651e5bab9
mEsUsah <stanley@haxor.no>
2025-11-06 11:51:07 +0000

    fix(OL-tutorial 1): add British National Grid layer

--- openLayers/resources/js/olTutorial_01.js
+++ openLayers/resources/js/olTutorial_01.js
@@ -14,7 +14,7 @@ map1 = new ol.Map({
     target: 'map_space'
 });
 
-
+// WMS overlay
 var wmsLayer = new ol.layer.Image({
     source: new ol.source.ImageWMS({
         url: 'https://ahocevar.com/geoserver/wms',
@@ -27,7 +27,7 @@ var wmsLayer = new ol.layer.Image({
 });
 map1.addLayer(wmsLayer);
 
-
+// GeoJSON overlay
 var geoJSONLayer = new ol.layer.Vector({
     source: new ol.source.Vector({
         format: new ol.format.GeoJSON(),
@@ -43,3 +43,21 @@ var geoJSONLayer = new ol.layer.Vector({
     })
 });
 map1.addLayer(geoJSONLayer);
+
+// British National Grid overlay
+proj4.defs('EPSG:27700', '+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 ' +
+    '+x_0=400000 +y_0=-100000 +ellps=airy ' +
+    '+towgs84=446.448,-125.157,542.06,0.15,0.247,0.842,-20.489 ' +
+    '+units=m +no_defs');
+var imageExtent = [0, 0, 700000, 1300000];
+
+var bngLayer = new ol.layer.Image({
+    source: new ol.source.ImageStatic({
+        url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/British_National_Grid.svg/2000px-British_National_Grid.svg.png',
+        projection: 'EPSG:27700',
+        imageExtent: imageExtent
+    }),
+    zIndex: 4,
+    opacity: 0.5,
+})
+map1.addLayer(bngLayer);
