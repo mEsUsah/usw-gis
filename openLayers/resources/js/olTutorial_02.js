b730eb8445df5e2819a310aab1a0cc435a33fd49
mEsUsah <stanley@haxor.no>
2025-11-07 18:09:49 +0000

    fix(OL-tutorial 2): add boilerplate

---
+++ openLayers/resources/js/olTutorial_02.js
@@ -0,0 +1,16 @@
+
+map1 = new ol.Map({
+    view: new ol.View({
+        // https://openlayers.org/en/v4.6.5/apidoc/ol.proj.html#.fromLonLat
+        center: ol.proj.fromLonLat([10.7522, 59.9139], 'EPSG:3857'), // Oslo, Norway, Google maps CRS
+        zoom: 7
+    }),
+    layers: [
+        new ol.layer.Tile({
+            source: new ol.source.OSM(),
+            zIndex: 1,
+        })
+    ],
+    target: 'map_space'
+});
+
