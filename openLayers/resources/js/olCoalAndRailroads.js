3ee9da82dbadcd5f58d8a6dc804e7f2c705dbb82
mEsUsah <stanley@haxor.no>
2025-11-29 19:19:17 +0000

    fix(openLayers): add railroads, and earthquakes

--- openLayers/resources/js/olCoalAndRailroads.js
+++ openLayers/resources/js/olCoalAndRailroads.js
@@ -143,3 +143,83 @@ function addLegend(featureColors) {
         legendElement.appendChild(listItem);
     }
 }
+
+// UK Railroads GeoJSON overlay
+const railroadsLayer = new ol.layer.Vector({
+    zIndex: 2,
+    opacity: 0.7,
+});
+
+const railroadsUrl = '/googleMapsApi/resources/geodata/railways_uk.geojson';
+fetch(railroadsUrl)
+    .then(response => response.json())
+    .then(data => {
+        railroadsLayer.setSource(new ol.source.Vector({
+            format: new ol.format.GeoJSON(),
+            url: railroadsUrl
+        }));
+
+        railroadsLayer.setStyle(new ol.style.Style({
+            stroke: new ol.style.Stroke({
+                color: 'brown',
+                width: 2,
+            })
+        }));
+    });
+
+const railroadsLayerButton = new OlButton('UK Railroads');
+railroadsLayerButton.element().addEventListener('click', function() {
+    if (railroadsLayerButton.isActive) {
+        map.removeLayer(railroadsLayer);
+        railroadsLayerButton.off();
+    } else {
+        map.addLayer(railroadsLayer);
+        railroadsLayerButton.on();
+    }
+});
+buttonWrapper.addButton(railroadsLayerButton.element());
+
+
+// Earthquakes GeoJSON overlay
+const earthquakesLayer = new ol.layer.Vector({
+    zIndex: 3,
+    opacity: 1,
+});
+
+// Custom marker
+var iconStyle = new ol.style.Style({
+    image: new ol.style.Icon({
+        anchor: [0.5, 1],           
+        anchorXUnits: 'fraction',
+        anchorYUnits: 'fraction',
+        src: '/resources/icons/mapMarker.svg',
+        scale: 1.0
+    })
+});
+
+// Load earthquake data and set source and style
+const earthquakesUrl = '/googleMapsApi/resources/geodata/2012_earthquakes_mag5.geojson';
+fetch(earthquakesUrl)
+    .then(response => response.json())
+    .then(data => {
+        earthquakesLayer.setSource(new ol.source.Vector({
+            format: new ol.format.GeoJSON(),
+            url: earthquakesUrl
+        }));
+
+        earthquakesLayer.setStyle((feature) => {
+            return iconStyle;
+        });
+    });
+
+const earthquakesLayerButton = new OlButton('Earthquakes');
+earthquakesLayerButton.element().addEventListener('click', function() {
+    if (earthquakesLayerButton.isActive) {
+        map.removeLayer(earthquakesLayer);
+        earthquakesLayerButton.off();
+    } else {
+        map.addLayer(earthquakesLayer);
+        earthquakesLayerButton.on();
+    }
+});
+buttonWrapper.addButton(earthquakesLayerButton.element());
