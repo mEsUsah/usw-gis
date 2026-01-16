dc5790ef6aa8643deda7777f179f208f2c7dc052
mEsUsah <stanley@haxor.no>
2026-01-16 16:42:36 +0000

    fix(geoserver WMS): add route points

--- geoserver/resources/js/routesWms.js
+++ geoserver/resources/js/routesWms.js
@@ -19,16 +19,27 @@ const mapLayer = L.tileLayer.wms('https://cache.kartverket.no/v1/wms?', { // htt
 mapLayer.addTo(map);
 
 
-// WMS layer from GeoServer
-const wmsLayer = L.tileLayer.wms('https://geoserver.haxor.no/geoserver/wms', {
-    layers: 'usw:morotur_routes',
+// WMS route layer from GeoServer
+const wmsRouteTrackLayer = L.tileLayer.wms('https://geoserver.haxor.no/geoserver/wms', {
+    layers: 'usw:morotur_route_tracks',
     format: 'image/png',
     attribution: '<a href="https://morotur.no/">Morotur.no</a>',
     transparent: true,
     opacity: 0.9
 });
-wmsLayer.setParams({cql_filter: "turtype='Fottur'"});
-wmsLayer.addTo(map);
+wmsRouteTrackLayer.setParams({cql_filter: "turtype='Fottur'"});
+wmsRouteTrackLayer.addTo(map);
+
+// WMS route point layer from GeoServer
+const wmsRoutePointLayer = L.tileLayer.wms('https://geoserver.haxor.no/geoserver/wms', {
+    layers: 'usw:morotur_route_points',
+    format: 'image/png',
+    attribution: '<a href="https://morotur.no/">Morotur.no</a>',
+    transparent: true,
+    opacity: 0.9
+});
+wmsRoutePointLayer.setParams({cql_filter: "type='0'"});
+wmsRoutePointLayer.addTo(map);
 
 // Buttons to toggle route types
 const hikingButton = document.getElementById("toggle_hiking");
@@ -42,7 +53,8 @@ hikingButton.addEventListener("click", () => {
     skiingButton.querySelector("[data-indicator]").classList.remove(toggleIndicatorClass);
     bikingButton.querySelector("[data-indicator]").classList.remove(toggleIndicatorClass);
     kayakingButton.querySelector("[data-indicator]").classList.remove(toggleIndicatorClass);
-    wmsLayer.setParams({cql_filter: "turtype='Fottur'"});
+    wmsRouteTrackLayer.setParams({cql_filter: "turtype='Fottur'"});
+    wmsRoutePointLayer.setParams({cql_filter: "type='0'"});
 });
 
 skiingButton.addEventListener("click", () => {
@@ -50,7 +62,8 @@ skiingButton.addEventListener("click", () => {
     skiingButton.querySelector("[data-indicator]").classList.add(toggleIndicatorClass);
     bikingButton.querySelector("[data-indicator]").classList.remove(toggleIndicatorClass);
     kayakingButton.querySelector("[data-indicator]").classList.remove(toggleIndicatorClass);
-    wmsLayer.setParams({cql_filter: "turtype='Skitur'"});
+    wmsRouteTrackLayer.setParams({cql_filter: "turtype='Skitur'"});
+    wmsRoutePointLayer.setParams({cql_filter: "type='1'"});
 });
 
 bikingButton.addEventListener("click", () => {
@@ -58,7 +71,8 @@ bikingButton.addEventListener("click", () => {
     skiingButton.querySelector("[data-indicator]").classList.remove(toggleIndicatorClass);
     bikingButton.querySelector("[data-indicator]").classList.add(toggleIndicatorClass);
     kayakingButton.querySelector("[data-indicator]").classList.remove(toggleIndicatorClass);
-    wmsLayer.setParams({cql_filter: "turtype='Sykkeltur'"});
+    wmsRouteTrackLayer.setParams({cql_filter: "turtype='Sykkeltur'"});
+    wmsRoutePointLayer.setParams({cql_filter: "type='2'"});
 });
 
 kayakingButton.addEventListener("click", () => {
@@ -66,8 +80,9 @@ kayakingButton.addEventListener("click", () => {
     skiingButton.querySelector("[data-indicator]").classList.remove(toggleIndicatorClass);
     bikingButton.querySelector("[data-indicator]").classList.remove(toggleIndicatorClass);
     kayakingButton.querySelector("[data-indicator]").classList.add(toggleIndicatorClass);
-    wmsLayer.setParams({cql_filter: "turtype='Padletur'"});
+    wmsRouteTrackLayer.setParams({cql_filter: "turtype='Padletur'"});
+    wmsRoutePointLayer.setParams({cql_filter: "type='3'"});
 });
 
-const legendUrl = "https://geoserver.haxor.no/geoserver/ows?service=WMS&version=1.3.0&request=GetLegendGraphic&format=image%2Fpng&width=20&height=20&layer=usw%3Amorotur_routes";
+const legendUrl = "https://geoserver.haxor.no/geoserver/ows?service=WMS&version=1.3.0&request=GetLegendGraphic&format=image%2Fpng&width=20&height=20&layer=usw%3Amorotur_route_tracks&style=morotur_grade";
 L.wmsLegend(legendUrl);
